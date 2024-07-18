5e6704561a6a7874f0d15ce4c74c8c2b29b83455
Yu Guo <grace.guo012@gmail.com>
2024-07-18 10:29:17 +0000

    Add about app views and link to project urls.py

--- about/views.py
+++ about/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponse
 # Create your views here.
+def about_me(request):
+    return HttpResponse("This is rendered by about app view.py file first funciton of about")
